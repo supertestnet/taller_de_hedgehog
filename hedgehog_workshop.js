945f2ef398a4225ebe00271a4a1fcadb8ad0a7a0
supertestnet <58400631+supertestnet@users.noreply.github.com>
2025-12-22 21:28:48 +0000

    Update hedgehog_workshop.js

--- hedgehog_workshop.js
+++ hedgehog_workshop.js
@@ -127,6 +127,8 @@ var hedgehog_workshop = {
         var channels_to_keep = {}
 
         //process all channels in state list
+        var confirmed_L2_balance = 0;
+        var unconfirmed_L2_balance = 0;
         var i; for ( i=0; i<Object.keys( hedgehog.state ).length; i++ ) {
             var chan_id = Object.keys( hedgehog.state )[ i ];
 
@@ -146,10 +148,13 @@ var hedgehog_workshop = {
 
             //otherwise, update the total balance and mark the channel for keeping
             var channel_balance = hedgehog.state[ chan_id ].alices_privkey ? hedgehog.state[ chan_id ].balances[ 0 ] || 0 : hedgehog.state[ chan_id ].balances[ 1 ] || 0;
-            if ( channel_is_confirmed ) hedgehog_workshop.confirmed_L2_balance = hedgehog_workshop.confirmed_L2_balance + channel_balance;
-            else hedgehog_workshop.unconfirmed_L2_balance = hedgehog_workshop.unconfirmed_L2_balance + channel_balance;
+            if ( channel_is_confirmed ) confirmed_L2_balance = confirmed_L2_balance + channel_balance;
+            else unconfirmed_L2_balance = unconfirmed_L2_balance + channel_balance;
             channels_to_keep[ chan_id ] = channel_balance;
         }
+
+        hedgehog_workshop.confirmed_L2_balance = confirmed_L2_balance;
+        hedgehog_workshop.unconfirmed_L2_balance = unconfirmed_L2_balance;
 
         var closing_channels = {}
 
@@ -185,7 +190,7 @@ var hedgehog_workshop = {
                 } else {
                     var num_of_confs = 0;
                     if ( info_i_seek.status.confirmed ) num_of_confs = blockheight - info_i_seek.status.block_height;
-                    awaited_blockheight = info_i_seek.status.block_height + 5;
+                    awaited_blockheight = info_i_seek.status.block_height || blockheight + 5;
                     waiting_for = `You are waiting for confirmations. Hedgehog force closures require two transactions to force close a channel; you've already broadcasted the first one, but the second one is timelocked and cannot be broadcasted til the first one has 5 confirmations. It currently has ${num_of_confs}, so you are waiting for ${5 - num_of_confs} confirmations. When the time is right, your app will broadcast this tx: <br><br>${tx_to_broadcast}`;
                     if ( num_of_confs >= 5 ) chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
                 }
@@ -219,8 +224,8 @@ var hedgehog_workshop = {
                     } else {
                         var num_of_confs = 0;
                         if ( info_i_seek.status.confirmed ) num_of_confs = blockheight - info_i_seek.status.block_height;
-                        awaited_blockheight = info_i_seek.status.block_height + 5;
-                        extra_awaited_blockheight = info_i_seek.status.block_height + 10;
+                        awaited_blockheight = info_i_seek.status.block_height || blockheight + 5;
+                        extra_awaited_blockheight = info_i_seek.status.block_height || blockheight + 10;
                         potential_extra_money = Number( tapscript.Tx.decode( tx_to_broadcast ).vout[ 0 ].value )  + 1_000;
                         waiting_for = `You are waiting for confirmations. Hedgehog force closures require two transactions to force close a channel; your counterparty already broadcasted the first one, but the second one is timelocked and cannot be broadcasted til the first one has 5 confirmations. It currently has ${num_of_confs}, so you are waiting for ${5 - num_of_confs} confirmations. When the time is right, you should see ${money_coming_to_you.toLocaleString()} sats arrive in your wallet. If your counterparty disappears and thus never finalizes the force closure, then after 10 confirmations, you get to broadcast a penalty tx that earns you ${( Number( tapscript.Tx.decode( tx_to_broadcast ).vout[ 0 ].value )  + 1_000 ).toLocaleString()} sats. Your app is waiting ${10 - num_of_confs} more confirmations from now to do that, but it probably won't happen if your counterparty is honest. This is the penalty tx:<br><br>${tx_to_broadcast}`;
                         if ( num_of_confs >= 10 ) chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
@@ -280,39 +285,33 @@ var hedgehog_workshop = {
 
         return { channels_to_keep, closing_channels }
     },
+    getChannelOpeningData: () => {
+        var privkey = hedgehog.bytesToHex( nobleSecp256k1.utils.randomPrivateKey() );
+        var pubkey = nobleSecp256k1.getPublicKey( privkey, true ).substring( 2 );
+        var preimage = hedgehog.bytesToHex( nobleSecp256k1.utils.randomPrivateKey() ).substring( 0, 32 );
+        var hash = hedgehog.rmd160( hedgehog.hexToBytes( preimage ) );
+        hedgehog.keypairs[ pubkey ] = {privkey, preimage};
+        var address = hedgehog_workshop.btc_address;
+        return [ pubkey, hash, address ];
+    },
     stateLoop: async ( auto = true ) => {
         var state_of_channels = await hedgehog_workshop.getStateOfChannels();
-        console.log( state_of_channels );
         //Show status of active channels
         var div = document.createElement( "div" );
         var unconfirmed_hh_balance = 0;
         var confirmed_hh_balance = 0;
         var channels_to_remove = [];
         
-        var i; for ( i=0; i<Object.keys( hedgehog.state ).length; i++ ) {
+        var i; for ( i=0; i<Object.keys( state_of_channels.channels_to_keep ).length; i++ ) {
             var chan_id = Object.keys( hedgehog.state )[ i ];
             var channel_label = hedgehog.state[ chan_id ].label;
-
-            //ensure channel still exists
-            var channel_utxos = await chain_client.commander( hedgehog_workshop.network_string.split( "," ), "utxos", hedgehog.state[ chan_id ].multisig );
-            var channel_ceased = true;
-            var channel_is_confirmed = false;
-            channel_utxos.forEach( utxo => {
-                if ( utxo.txid !== hedgehog.state[ chan_id ].multisig_utxo_info.txid ) return;
-                if ( utxo.status.confirmed ) channel_is_confirmed = true;
-                channel_ceased = false;
-            });
-            if ( channel_ceased ) {
-                channels_to_remove.push( chan_id );
-                continue;
-            }
 
             var inner = document.createElement( "div" );
             var html = `
                 <div>
                     <p>Label: <span class="channel_label">loading...</span></p>
                     <p>Balance: <span class="channel_balance">0</span> sats</p>
-                    <p><button class="close_channel">Close channel</button> <button class="channel_send_btn">Send</button> <button class="channel_receive_btn">Receive</button></p>
+                    <p><button class="close_channel">Close channel</button> <button class="channel_send_btn hidden">Send</button> <button class="channel_receive_btn">Receive</button></p>
                 </div>
             `;
             inner.innerHTML = html;
@@ -323,8 +322,6 @@ var hedgehog_workshop = {
             inner.setAttribute( "data-chan_id", chan_id );
             inner.getElementsByClassName( "channel_label" )[ 0 ].innerText = channel_label;
             var channel_balance = hedgehog.state[ chan_id ].alices_privkey ? hedgehog.state[ chan_id ].balances[ 0 ] || 0 : hedgehog.state[ chan_id ].balances[ 1 ] || 0;
-            if ( channel_is_confirmed ) confirmed_hh_balance = confirmed_hh_balance + channel_balance;
-            else unconfirmed_hh_balance = unconfirmed_hh_balance + channel_balance;
             inner.getElementsByClassName( "channel_balance" )[ 0 ].innerText = channel_balance.toLocaleString();
             inner.getElementsByClassName( "close_channel" )[ 0 ].onclick = async () => {
                 var force_close_txs = await hedgehog.closeChannel( chan_id );
@@ -349,72 +346,12 @@ var hedgehog_workshop = {
         }
 
         //show status of closing channels
-        var i; for ( i=0; i<Object.keys( hedgehog_workshop.channels_being_closed ).length; i++ ) {
+        var i; for ( i=0; i<Object.keys( state_of_channels.closing_channels ).length; i++ ) {
             var chan_id = Object.keys( hedgehog_workshop.channels_being_closed )[ i ];
-            var channel_data = hedgehog_workshop.channels_being_closed[ chan_id ];
 
             //prepare variables
-            var channel_label = channel_data.label;
-            var money_coming_to_you = channel_data.money_coming_to_you;
-            if ( channel_data.hasOwnProperty( "force_close_txs" ) ) {
-                var tx_to_broadcast = channel_data.force_close_txs[ 1 ];
-                var type_of_tx = "finalization_tx";
-                var blockheight = await chain_client.commander( hedgehog_workshop.network_string.split( "," ), "blockheight" );
-                var txid_of_tx0 = channel_data.txid_of_tx0;
-                var revocable_address = channel_data.revocable_address;
-                var conf_data = await chain_client.commander( hedgehog_workshop.network_string.split( "," ), "utxos", revocable_address );
-                var info_i_seek = null;
-                conf_data.every( utxo => {
-                    if ( utxo.txid !== txid_of_tx0 ) return true;
-                    info_i_seek = utxo;
-                });
-                if ( !info_i_seek || !info_i_seek.hasOwnProperty( "status"  ) || !info_i_seek.status.hasOwnProperty( "confirmed" ) ) {
-                    chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
-                    var waiting_for = `Nothing! The transaction corresponding to your latest state was broadcasted and all is well. You should see ${money_coming_to_you.toLocaleString()} sats arrive in your wallet any second.`;
-                } else {
-                    var num_of_confs = 0;
-                    if ( info_i_seek.status.confirmed ) num_of_confs = blockheight - info_i_seek.status.block_height;
-                    var waiting_for = `You are waiting for confirmations. Hedgehog force closures require two transactions to force close a channel; you've already broadcasted the first one, but the second one is timelocked and cannot be broadcasted til the first one has 5 confirmations. It currently has ${num_of_confs}, so you are waiting for ${5 - num_of_confs} confirmations. When the time is right, your app will broadcast this tx: <br><br>${tx_to_broadcast}`;
-                    if ( num_of_confs >= 5 ) chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
-                }
-            } else {
-                var tx_to_broadcast = null;
-                var type_of_tx = null;
-                var waiting_for = null;
-                if ( channel_data.force_close_data.hasOwnProperty( "full_revocation_tx" ) ) {
-                    tx_to_broadcast = channel_data.force_close_data.full_revocation_tx;
-                    type_of_tx = "full_revocation_tx";
-                    chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
-                    money_coming_to_you = Number( tapscript.Tx.decode( tx_to_broadcast ).vout[ 0 ].value );
-                    waiting_for = `Nothing! Your counterparty broadcasted very old state so we broadcasted the penalty tx and you should see ${money_coming_to_you.toLocaleString()} sats arrive in your wallet any second.`;
-                } else if ( channel_data.force_close_data.hasOwnProperty( "conditional_revocation_tx" ) ) {
-                    tx_to_broadcast = channel_data.force_close_data.conditional_revocation_tx;
-                    type_of_tx = "conditional_revocation_tx";
-                    chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
-                    waiting_for = `Nothing! Your counterparty broadcasted the transaction corresponding to his or her most recent state, which does not quite match yours, so you updated the transaction to your latest one. You should see ${money_coming_to_you.toLocaleString()} sats arrive in your wallet any second.`;
-                } else {
-                    tx_to_broadcast = channel_data.force_close_data.timeout_tx;
-                    type_of_tx = "timeout_tx";
-                    var blockheight = await chain_client.commander( hedgehog_workshop.network_string.split( "," ), "blockheight" );
-                    var txid_of_tx0 = channel_data.txid_of_tx0;
-                    var revocable_address = channel_data.revocable_address;
-                    var conf_data = await chain_client.commander( hedgehog_workshop.network_string.split( "," ), "utxos", revocable_address );
-                    var info_i_seek = null;
-                    conf_data.every( utxo => {
-                        if ( utxo.txid !== txid_of_tx0 ) return true;
-                        info_i_seek = utxo;
-                    });
-                    if ( !info_i_seek || !info_i_seek.hasOwnProperty( "status"  ) || !info_i_seek.status.hasOwnProperty( "confirmed" ) ) {
-                        chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
-                        var waiting_for = `Nothing! The transaction corresponding to your latest state was broadcasted and all is well. You should see ${money_coming_to_you.toLocaleString()} sats arrive in your wallet any second.`;
-                    } else {
-                        var num_of_confs = 0;
-                        if ( info_i_seek.status.confirmed ) num_of_confs = blockheight - info_i_seek.status.block_height;
-                        waiting_for = `You are waiting for confirmations. Hedgehog force closures require two transactions to force close a channel; your counterparty already broadcasted the first one, but the second one is timelocked and cannot be broadcasted til the first one has 5 confirmations. It currently has ${num_of_confs}, so you are waiting for ${5 - num_of_confs} confirmations. When the time is right, you should see ${money_coming_to_you.toLocaleString()} sats arrive in your wallet. If your counterparty disappears and thus never finalizes the force closure, then after 10 confirmations, you get to broadcast a penalty tx that earns you ${( Number( tapscript.Tx.decode( tx_to_broadcast ).vout[ 0 ].value )  + 1_000 ).toLocaleString()} sats. Your app is waiting ${10 - num_of_confs} more confirmations from now to do that, but it probably won't happen if your counterparty is honest. This is the penalty tx:<br><br>${tx_to_broadcast}`;
-                        if ( num_of_confs >= 10 ) chain_client.commander( hedgehog_workshop.network_string.split( "," ), "broadcast", tx_to_broadcast );
-                    }
-                }
-            }
+            var channel_label = hedgehog_workshop.channels_being_closed[ chan_id ].label;
+            var { money_coming_to_you, waiting_for } = state_of_channels.closing_channels[ chan_id ];
 
             var inner = document.createElement( "div" );
             var html = `
@@ -450,51 +387,9 @@ var hedgehog_workshop = {
             else $( '.channels_list' ).append( div.children[ 0 ] );
         }
 
-        //Remove any closed channels
-        var i; for ( i=0; i<channels_to_remove.length; i++ ) {
-            var chan_id = channels_to_remove[ i ];
-            if ( hedgehog.state[ chan_id ].i_force_closed ) {
-                //you are the person who broadcasted the tx
-                var i_am_alice = !!hedgehog.state[ chan_id ].alices_privkey;
-                var money_coming_to_you = i_am_alice ? hedgehog.state[ chan_id ].balances[ 0 ] : hedgehog.state[ chan_id ].balances[ 1 ];
-                var force_close_txs = hedgehog.state[ chan_id ].latest_force_close_txs;
-                var label = hedgehog.state[ chan_id ].label;
-                var txid_of_tx0 = tapscript.Tx.util.getTxid( hedgehog.state[ chan_id ].latest_force_close_txs[ 0 ] );
-                var spky = tapscript.Tx.decode( hedgehog.state[ chan_id ].latest_force_close_txs[ 0 ] ).vout[ 0 ].scriptPubKey;
-                var revocable_address = tapscript.Address.fromScriptPubKey( spky, hedgehog.network );
-                hedgehog_workshop.channels_being_closed[ chan_id ] = { label, force_close_txs, i_am_alice, money_coming_to_you, txid_of_tx0, revocable_address };
-                delete hedgehog.state[ chan_id ];
-            } else {
-                //find out if your counterparty broadcasted the right tx
-                var closure_txids = Object.keys( hedgehog.state[ chan_id ].txids_to_watch_for );
-                var force_close_data = null;
-                var j; for ( j=0; j<closure_txids.length; j++ ) {
-                    var tx_exists = await chain_client.commander( hedgehog_workshop.network_string.split( "," ), "rawtx", closure_txids[ i ] );
-                    if ( !hedgehog.isValidHex( tx_exists ) ) continue;
-                    force_close_data = { ...hedgehog.state[ chan_id ].txids_to_watch_for[ closure_txids[ i ] ], txid: closure_txids[ i ]} ;
-                    break;
-                }
-                if ( force_close_data ) {
-                    var i_am_alice = !!hedgehog.state[ chan_id ].alices_privkey;
-                    var money_coming_to_you = i_am_alice ? hedgehog.state[ chan_id ].balances[ 0 ] : hedgehog.state[ chan_id ].balances[ 1 ];
-                    var label = hedgehog.state[ chan_id ].label;
-                    var multisig = hedgehog.state[ chan_id ].multisig;
-                    var txid_of_tx0 = force_close_data.txid;
-                    var spky = tapscript.Tx.decode( force_close_data.tx0 ).vout[ 0 ].scriptPubKey;
-                    var revocable_address = tapscript.Address.fromScriptPubKey( spky, hedgehog.network );
-                    hedgehog_workshop.channels_being_closed[ chan_id ] = { label, force_close_data, i_am_alice, money_coming_to_you, txid_of_tx0, revocable_address };
-                    delete hedgehog.state[ chan_id ];
-                }
-            }
-        }
-
-        //Update hedgehog balance
-        hedgehog_workshop.confirmed_L2_balance = confirmed_hh_balance;
-        hedgehog_workshop.unconfirmed_L2_balance = unconfirmed_hh_balance;
-        $( '.confirmed_hedgehog_balance' ).innerText = confirmed_hh_balance.toLocaleString();
-        $( '.unconfirmed_hedgehog_balance' ).innerText = unconfirmed_hh_balance.toLocaleString();
-
-        //Update L1 balances
+        //Update balances
+        $( '.confirmed_hedgehog_balance' ).innerText = hedgehog_workshop.confirmed_L2_balance.toLocaleString();
+        $( '.unconfirmed_hedgehog_balance' ).innerText = hedgehog_workshop.unconfirmed_L2_balance.toLocaleString();
         var { confirmed_balance, unconfirmed_balance } = await hedgehog_workshop.getBalances();
         hedgehog_workshop.confirmed_L1_balance = confirmed_balance;
         hedgehog_workshop.unconfirmed_L1_balance = unconfirmed_balance;
@@ -502,14 +397,5 @@ var hedgehog_workshop = {
         $( '.unconfirmed_balance' ).innerText = unconfirmed_balance.toLocaleString();
         await chain_client.waitSomeTime( 5_000 );
         if ( auto ) hedgehog_workshop.stateLoop();
-    },
-    getChannelOpeningData: () => {
-        var privkey = hedgehog.bytesToHex( nobleSecp256k1.utils.randomPrivateKey() );
-        var pubkey = nobleSecp256k1.getPublicKey( privkey, true ).substring( 2 );
-        var preimage = hedgehog.bytesToHex( nobleSecp256k1.utils.randomPrivateKey() ).substring( 0, 32 );
-        var hash = hedgehog.rmd160( hedgehog.hexToBytes( preimage ) );
-        hedgehog.keypairs[ pubkey ] = {privkey, preimage};
-        var address = hedgehog_workshop.btc_address;
-        return [ pubkey, hash, address ];
     },
 }
